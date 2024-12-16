4cffe38ffad4b7a030fb6e118adee5cceb535f25
MarineGiroux <85103261+MarineGiroux@users.noreply.github.com>
2024-12-16 14:49:59 +0100

    quest-03

---
+++ Variable/Indi.java
@@ -0,0 +1,12 @@
+package Variable;
+
+public class Indi {
+    public static void main(String[] args) {
+        String nameMovie = "Indiana Jones and the Last Crusade";
+        boolean isSeen = true;
+        int yearOfRelease = 1989;
+        float movieRating = 8.2f;
+
+        System.out.println(nameMovie + " est sortie en " + yearOfRelease + ". il est noté " + movieRating + " sur 10 sur IMDB. Et est ce que je l'ai vu ? " + isSeen);
+    }
+}
